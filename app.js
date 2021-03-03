4a29725a723d28eebb5934497733999ff44c41c9
Ariel Aizemberg <aaizemberg@users.noreply.github.com>
2021-03-03 18:41:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var url = 'https://cors-anywhere.herokuapp.com/https://docs.google.com/spreadsheets/d/e/2PACX-1vTmFho6ie3IDsHrZcfOASnA4apB5fRbh3HET3dvP_lwd-4ZmMBea66QuhTZI82oD_j-0_BBdsh5a-qb/pub?gid=0&single=true&output=tsv';
+var url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTmFho6ie3IDsHrZcfOASnA4apB5fRbh3HET3dvP_lwd-4ZmMBea66QuhTZI82oD_j-0_BBdsh5a-qb/pub?gid=0&single=true&output=tsv';
 
 d3.tsv( url, function(data) {
   var figures = d3.select("div#news").selectAll("figure").data(data).enter().append("figure");
